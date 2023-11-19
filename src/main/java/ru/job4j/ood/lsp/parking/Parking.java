cd6943a5eecf12d49323f6452ee043e82ce6b260
Rubezzz <inigor1@mail.ru>
2023-11-19 18:13:33 +0700

    2. Парковка машин [#853]

---
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -0,0 +1,12 @@
+package ru.job4j.ood.lsp.parking;
+
+import java.util.List;
+
+public interface Parking {
+
+    void add(Car car);
+
+    void remove(Car car);
+
+    List<Car> listAllCar();
+}
---
+++ src/main/java/ru/job4j/ood/lsp/parking/ParkingSpace.java
@@ -0,0 +1,6 @@
+package ru.job4j.ood.lsp.parking;
+
+public interface ParkingSpace {
+
+    void add(Car car);
+}
